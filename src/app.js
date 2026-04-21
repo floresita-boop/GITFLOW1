e4aa25ad3dc89900d6a75edef65d29d314bf5529
florelia <frisbyfloresita@gmail.com>
2026-04-20 19:32:48 -0500

    hotfix: corregir factorial

--- src/app.js
+++ src/app.js
@@ -22,3 +22,12 @@ export function healthCheck(){
     version: '1.0.0'
   };
 }
+export function factorial(n){
+    let resultado = 1;
+    for (let i = 1; i <= n; i++) {
+        resultado *= i;
+    }
+    return resultado;
+}
+  
+
